2ffafb3bc408165a726edfd7b04c15e0b79c5570
BlackMoon <belchikov1996@mail.ru>
2015-02-14 14:04:29 +0300

    Adding required gems. Just for fun.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostcardSendingService::Application.config.secret_key_base = '735dda3042a671420ce929c4080dde2dc90fabf4bf4950d8b846f64cf0244c716937613e33cbf4d15c91abf02563cad6cc85946d5719073ec075772fdb1bafb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PostcardSendingService::Application.config.session_store :cookie_store, key: '_postcard-sending-service_session'
